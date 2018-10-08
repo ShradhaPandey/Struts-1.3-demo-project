ead055d95379336bce35d3a72b6a5fd98f4b9546
Shradha Pandey <35809333+ShradhaPandey@users.noreply.github.com>
2018-10-08 03:16:40 +0200

    Update HelloUserAction.java

--- Struts1.3DemoProject/src/shradha/struts/demo/action/HelloUserAction.java
+++ Struts1.3DemoProject/src/shradha/struts/demo/action/HelloUserAction.java
@@ -25,11 +25,23 @@ public class HelloUserAction extends Action{
 		
 		if(helloUserForm!=null && helloUserForm.getUserName().equalsIgnoreCase("Shradha")) {
 			fw = mapping.findForward("success");
+			/*
+			 <forward name="success" path="/welcomeUser.jsp"> this is mentioned in
+			 struts-config's --> action-mapping's --> action's --> forward tag
+			 Thus the struts servlet knows that when it needs to redirect to welcomeUser.jsp page.
+			 
+			
+			*/
 		}
 		else {
 			ActionErrors actionErrors = new ActionErrors();
-			actionErrors.add("", new ActionMessage("helloUser.msg.error"));
+			actionErrors.add("", new ActionMessage("helloUser.msg.error"));  
 			saveErrors(request, actionErrors);
+			/*
+			 this "helloUser.msg.error" is key value from MessageBundle.propeties file
+			 the struts application knows that it needs to lookup in MessageBundle.propeties file
+			 because the file name is given in message-resource tag in struts-config.xml file
+			*/
 		}
 			
 		return fw;
